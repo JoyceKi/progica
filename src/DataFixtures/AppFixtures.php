1ce43c8ef571e44719b04a711ecdbff2bc5a0dc3
JoyceKi <jocelyne.kim18@gmail.com>
2023-12-06 13:24:25 +0100

    Données insérées dans la bdd avec DataFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,11 +2,15 @@
 
 namespace App\DataFixtures;
 
+use DateTime;
 use App\Entity\Gite;
 use App\Entity\Adresse;
+use App\Entity\Periode;
 use App\Entity\Service;
+use App\Entity\TarifGite;
 use App\Entity\Equipement;
 use App\Entity\Proprietaire;
+use App\Entity\Disponibilites;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -18,10 +22,10 @@ class AppFixtures extends Fixture
         $proprietaires = [];
         for ($i = 0; $i < 5; $i++) {
             $proprietaire = new Proprietaire();
-            $proprietaire->setNom("Kim");
-            $proprietaire->setPrenom("Joyce");
-            $proprietaire->setTelephone("0123456789");
-            $proprietaire->setEmail("exemple@exemple.com");
+            $proprietaire->setNom("Danton");
+            $proprietaire->setPrenom("Frédéric");
+            $proprietaire->setTelephone("0126456789");
+            $proprietaire->setEmail("example@example.com");
             $manager->persist($proprietaire);
             $proprietaires[] = $proprietaire;
         }
@@ -32,20 +36,20 @@ class AppFixtures extends Fixture
             for ($i = 0; $i < 2; $i++) {
                 $gite = new Gite();
                 // les attributs des gîtes
-                $gite->setNom("Nuage");
-                $gite->setChambre(3);
-                $gite->setCouchage(5);
-                $gite->setSurfaceHab(70);
-                $gite->setAnimauxacceptes(1);
-                $gite->setTarifAnimaux(25);
+                $gite->setNom("Sagoa");
+                $gite->setChambre(2);
+                $gite->setCouchage(4);
+                $gite->setSurfaceHab(90);
+                $gite->setAnimauxacceptes(0);
+                $gite->setTarifAnimaux(0.0);
                 
                 // associer le gîte au propriétaire
                 $gite->setProprietaire($proprietaire);
 
                 // j'ajoute des équipements dans la boucle pour qu'ils soient associés aux gîtes
                 $equipement = new Equipement();
-                $equipement->setNom("Lave-vaisselle");
-                $equipement->setDescription("8 couverts");
+                $equipement->setNom("Lave-linge");
+                $equipement->setDescription("7 kg");
                 $equipement->setTarif(0.0);
 
                 $manager->persist($equipement);
@@ -53,26 +57,63 @@ class AppFixtures extends Fixture
 
                 // j'ajoute des services
                 $service = new Service();
-                $service->setNom("Location de linge");
-                $service->setDescription("Draps, serviettes de bain, serviettes de toilette");
-                $service->setTarif(15);
+                $service->setNom("Ménage de fin de séjour");
+                $service->setDescription("Poussière, aspirateur, sanitaires, poubelles");
+                $service->setTarif(35);
                 $manager->persist($service);
                 $gite->addService($service);
 
+                // j'ajoute les adresses
                 $adresse = new Adresse();
-                $adresse->setVoie("Lieu-dit Le Layon");
-                $adresse->setCodePostal("49750");
-                $adresse->setVille("Val du Layon");
-                $adresse->setDepartement("Maine et Loire");
-                $adresse->setRegion("Pays de la Loire");
-                $adresse->setLatitude(455);
-                $adresse->setLongitude(460);
+                $adresse->setVoie("Rue du Pays");
+                $adresse->setCodePostal("35500");
+                $adresse->setVille("Vitré");
+                $adresse->setDepartement("Ille et Vilaine");
+                $adresse->setRegion("Bretagne");
+                $adresse->setLatitude(48.133);
+                $adresse->setLongitude(-1.2);
                 $manager->persist($adresse);
                 $gite->setAdresse($adresse);
 
-                $manager->persist($gite);              
+                // j'ajoute les tarifs
+                $tarif = new TarifGite();
+                $tarif->setTarifHebdo(550);
+                $manager->persist($tarif);
+                $gite->addTarifGite($tarif);
+
+                $manager->persist($gite);  
+                // j'ajoute les disponibilités et les associe aux propriétaires
+                $dispo = new Disponibilites();
+                $dispo->setJours("Du lundi au vendredi");
+
+                // Création d'un objet DateTime pour heureDebut
+                $heureDebut = DateTime::createFromFormat('H:i', '09:00');
+                // j'utilise l'objet $heureDebut 
+                $dispo->setHeureDebut($heureDebut);
+
+                // Création d'un objet DateTime pour heureFin
+                $heureFin = DateTime::createFromFormat('H:i', '18:00');
+                // j'utilise l'objet $heureFin 
+                $dispo->setHeureFin($heureFin);
+                $proprietaire->addDisponibilite($dispo);
+                $manager->persist($dispo);
+
             }
         }
+
+        // j'ajoute les périodes et les associe aux tarifs de gîtes
+        $periode = new Periode();
+        $periode->setNom("Haute saison");
+
+        // Création objets DateTime pour dateDebut et dateFin
+        $dateDebut = DateTime::createFromFormat('Y-m-d', '2024-06-15');
+        $periode->setDateDebut($dateDebut);
+
+        $dateFin = DateTime::createFromFormat('Y-m-d', '2024-09-30');
+        $periode->setDateFin($dateFin);
+        $tarif->addPeriode($periode);
+        $manager->persist($periode);
+        
         // Après avoir tout ajouté, je flush les données dans la bdd
         $manager->flush();
     }
